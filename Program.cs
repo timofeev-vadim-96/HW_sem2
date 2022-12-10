41b444dbbddc96eb9981acbe6e6536eb2313d42d
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-10 23:13:08 +0300

    task2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
-﻿// Программа, которая принимет на вход трехзначное число и на выходе показывает вторую цифру этого числа.
-System.Console.WriteLine("Введите трехзначное число");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-int intermediateValue = firstNumber%100;
-int result = intermediateValue/10;
-System.Console.WriteLine("Второе число это " + result);
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+System.Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number<100)
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    if (Number<1000)
+    {
+        int result1 = Number%100%10; 
+        System.Console.WriteLine(result1);
+    }
+    else
+    {
+        if (Number < 10000)
+        {
+        int result2 = Number%1000%100/10;
+        System.Console.WriteLine(result2);
+        }
+        else
+        {
+            System.Console.WriteLine("ваше число > 10.000 и этот вариант не проработан");
+        }
+    }
+}
