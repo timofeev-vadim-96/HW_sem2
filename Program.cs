57a0807f263d0d53dfce6e8eb72c267e4f0904ad
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-10 23:00:40 +0300

    task1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿// Hi there! Switch branches to see homework tasks
+﻿// Программа, которая принимет на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите трехзначное число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int intermediateValue = firstNumber%100;
+int result = intermediateValue/10;
+System.Console.WriteLine("Второе число это " + result);
